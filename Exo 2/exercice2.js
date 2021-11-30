cdbed43d969acb2cb18d14e3e5c562c93813fa50
devyosse <narutoshippuden884@gmail.com>
2021-11-30 11:53:11 +0100

    exo 85

--- Exo 2/exercice2.js
+++ Exo 2/exercice2.js
@@ -1,11 +1,11 @@
 let personne = {
     prenom : "James",
     nom : "Bond",
-           :
+    métier: "Agent secret",
 };
 
 /* Afficher ci dessous la propriété metier de notre objet dans un alert */
 
-alert();
+alert(personne["métier"]);
 
 
--- Exo 3/exercice3.js
+++ Exo 3/exercice3.js
@@ -1,13 +1,14 @@
 let personne = {
-    "prenom" : "James",
-    "nom" : "Bond",
-           :    ,
-            :
+    prenom : "James",
+    nom : "Bond",
+    nomDeCode : "007",
 };
 
 /* Afficher ci dessous "mon nom est <propriété nom de notre objet>, <propriété prenom de notre objet>
  <propriété nom de notre objet>, agent <propriété nomDeCode de notre objet>" dans un alert */
 
-alert();
+alert(personne.prenom + " " + personne.nom + " " + personne.nomDeCode + "");
+
+console.log(alert)
 
 
